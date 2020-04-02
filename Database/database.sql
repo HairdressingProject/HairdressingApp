6a010c0f0ddefea37a0620165e6aaf56ff1658b1
ggornes <gerardogornes@gmail.com>
2020-04-02 17:13:15 +0800

    created COLOURS table | seeded face_shapes and skin_tones tables as described by the client

--- Database/database.sql
+++ Database/database.sql
@@ -38,7 +38,7 @@ ENGINE = INNODB;
 -- USER_FEATURES table
 CREATE TABLE IF NOT EXISTS hair_project_db.user_features
 (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    `id`             BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
     `user_id`        BIGINT UNSIGNED NOT NULL DEFAULT 0,
     `face_shape_id`  BIGINT NOT NULL,
     `skin_tone_id`   BIGINT NOT NULL,
@@ -59,10 +59,10 @@ ENGINE = INNODB;
 -- FACE_SHAPES table
 CREATE TABLE IF NOT EXISTS hair_project_db.face_shapes
 (
-    `id`             BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-    `shape_name`     VARCHAR(128) NOT NULL DEFAULT '** ERROR: missing category **',
-    `date_createdt`  DATETIME NOT NULL DEFAULT NOW(),
-    `date_modified`  DATETIME DEFAULT NULL ON UPDATE NOW(),
+    `id`            BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    `shape_name`    VARCHAR(128) NOT NULL DEFAULT '** ERROR: missing category **',
+    `date_createdt` DATETIME NOT NULL DEFAULT NOW(),
+    `date_modified` DATETIME DEFAULT NULL ON UPDATE NOW(),
     INDEX (`id`)
 )
 CHARACTER SET utf8mb4
@@ -72,7 +72,7 @@ ENGINE = INNODB;
 -- FACE_SHAPE_LINKS table
 CREATE TABLE IF NOT EXISTS hair_project_db.face_shape_links
 (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    `id`            BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
     `face_shape_id` BIGINT UNSIGNED NOT NULL,
     `link_name`     VARCHAR(128) NOT NULL DEFAULT '** ERROR: missing category **',
     `link_url`      VARCHAR(512) NOT NULL DEFAULT '** ERROR: missing category **',
@@ -152,10 +152,10 @@ ENGINE = INNODB;
 -- SKIN_TONES table
 CREATE TABLE IF NOT EXISTS hair_project_db.skin_tones
 (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    `id`             BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
     `skin_tone_name` VARCHAR(128) NOT NULL DEFAULT '** ERROR: missing category **',
-    `date_createdt`    DATETIME NOT NULL DEFAULT NOW(),
-    `date_modified`    DATETIME DEFAULT NULL ON UPDATE NOW(),
+    `date_createdt`  DATETIME NOT NULL DEFAULT NOW(),
+    `date_modified`  DATETIME DEFAULT NULL ON UPDATE NOW(),
     INDEX (`id`)
 )
 CHARACTER SET utf8mb4
@@ -180,8 +180,36 @@ CHARACTER SET utf8mb4
 COLLATE utf8mb4_general_ci
 ENGINE = INNODB;
 
+-- COLOURS table
+CREATE TABLE IF NOT EXISTS hair_project_db.colours
+(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    `colour_name`   VARCHAR(64) NOT NULL DEFAULT '** ERROR: missing category **',
+    `colour_hash`   VARCHAR(64) NOT NULL DEFAULT '** ERROR: missing category **',
+    `date_createdt` DATETIME NOT NULL DEFAULT NOW(),
+    `date_modified` DATETIME DEFAULT NULL ON UPDATE NOW(),
+    INDEX(`id`)
+)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci
+ENGINE = INNODB;
+
+
+
+-- Seed face_shapes table as described by the client
+INSERT INTO hair_project_db.face_shapes(`shape_name`)
+VALUES
+    ('heart_shaped'), ('square'), ('rectangular'), ('diamond'), ('triangular'), ('inverted_triangular'), ('round'), ('oval'), ('oblong')
+;
+
+-- Seed skin_tones table as described by the client
+INSERT INTO hair_project_db.skin_tones(`skin_tone_name`)
+VALUES
+    ('very_fair'), ('fair'), ('olive'), ('light_brown'), ('brown'), ('dark_brown'), ('black_brown')
+;
+
+
 
 -- TODO
--- COLOURS table
 -- Create skin_tone_colour_id on SKIN_TONES table and link it co COLOURS table
 -- Seed tables
