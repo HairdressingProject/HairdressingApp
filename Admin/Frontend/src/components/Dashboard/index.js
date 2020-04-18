9d9b8236c88b62eb73173eea89567d9434bd1034
ggornes <gerardogornes@gmail.com>
2020-04-18 12:20:03 +0800

    testing a modal popup on dashboard page

--- Admin/Frontend/src/components/Dashboard/index.js
+++ Admin/Frontend/src/components/Dashboard/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './Dashboard.scss';
 
+import 'foundation-sites/js/foundation';
+import 'foundation-sites/js/foundation.reveal';
+
+// import { MenuModal } from "../Menu2/MenuModal"
+import { Reveal } from "../Navigation/Menu2/Reveal"
+
+
 export const Dashboard = () => (
     <div className="dashboard-container">
         dashboard
+
+        <div className="reveal" id="exampleModal1" data-reveal>
+        <h1>Awesome. I am a Reveal</h1>
+        </div>
+        
+        <div>
+            <a data-open="exampleModal1">click</a>
+            <br></br>
+            <a data-open="item-modal">click2</a>
+            <Reveal />
+        </div>
+        
+
     </div>
 );
