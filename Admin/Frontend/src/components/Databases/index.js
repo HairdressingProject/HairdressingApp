301f27bda206a8d6071344ca12033e9c2dd6ee29
ggornes <gerardogornes@gmail.com>
2020-04-21 18:10:17 +0800

    Users table display on Database page

--- Admin/Frontend/src/components/Databases/index.js
+++ Admin/Frontend/src/components/Databases/index.js
@@ -1,27 +1,59 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 
+import { Table } from 'react-foundation-components';
+
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export const Databases = () => 
     {
-        const [data, setData] = useState({hits: []});
+        
+        const [data, setData] = useState([]);
 
         useEffect(async () => {
             const result = await axios(
-                'https://hn.algolia.com/api/v1/search?query=redux'
+                //'https://128.199.233.190:5001/api/Users'
+                'https://localhost:5001/api/Users'
             );
+            console.log("Result:");
+            console.log(result);
+
+            console.log("Result.data:");
+            console.log(result.data);
+            
+            
             setData(result.data);
         }, []);
 
         return(
             <div>
                 databases
-                <ul>
-                    {data.hits.map(item => (
-                        <li key={item.objectID}>
-                        <a href={item.url}>{item.title}</a>
-                        </li>
-                    ))}
-                </ul>
+
+                <Table scroll>
+                    <thead>
+                        <tr>
+                            <th>User id</th>
+                            <th>User Name</th>
+                            <th>User First Name</th>
+                            <th>User Last Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(item => (
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.userName}</td>
+                                <td>{item.firstName}</td>
+                                <td>{item.lastName}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+
+
+
+
+                
             </div>
         )
 
