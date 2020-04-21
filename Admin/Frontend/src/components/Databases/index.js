0708c839ce5a74dd4519ddbfa50615824b285a8a
ggornes <gerardogornes@gmail.com>
2020-04-21 15:25:54 +0800

    testing data fetching on Databases page

--- Admin/Frontend/src/components/Databases/index.js
+++ Admin/Frontend/src/components/Databases/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
-export const Databases = () => (
-    <div>
-        databases
-    </div>
-);
+export const Databases = () => 
+    {
+        const [data, setData] = useState({hits: []});
+
+        useEffect(async () => {
+            const result = await axios(
+                'https://hn.algolia.com/api/v1/search?query=redux'
+            );
+            setData(result.data);
+        });
+
+        return(
+            <div>
+                databases
+                <ul>
+                    {data.hits.map(item => (
+                        <li key={item.objectID}>
+                        <a href={item.url}>{item.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+
+};
