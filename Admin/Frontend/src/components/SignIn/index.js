71ffd04c85a9db4427dfcced22acdc037ef4e4b7
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2020-04-22 20:19:57 +0800

    added initial setup for form validation

--- Admin/Frontend/src/components/SignIn/index.js
+++ Admin/Frontend/src/components/SignIn/index.js
@@ -22,13 +22,21 @@ export const SignIn = () => {
             label: 'Username or email',
             input: '',
             type: 'text',
-            validation: []
+            validation: {
+                errors: true,
+                errorMessages: [
+                    "Invalid username"
+                ]
+            }
         },
         {
             label: 'Password',
             input: '',
             type: 'password',
-            validation: []
+            validation: {
+                errors: false,
+                errorMessages: []
+            }
         },
         {
             label: 'Remember Me',
@@ -49,6 +57,12 @@ export const SignIn = () => {
         setFormFields(currentFormFields);
     };
 
+    const signInSubmitButtonClasses = ["signin-form-submit-button"];
+
+    if (formFields.some(f => f.validation.errors)) {
+        signInSubmitButtonClasses.push("signin-form-submit-button-invalid")
+    }
+
     return (
         <div className="signin-container text-center">
             <div className="signin-form">
@@ -67,75 +81,101 @@ export const SignIn = () => {
                     </Column>
                 </Row>
 
-                {
-                    formFields.map((field, index) => {
+                <form action="#">
+                    {
+                        formFields.map((field, index) => {
 
-                        if (field.type === 'checkbox') {
-                            return (
-                                <FormField key={index} className="signin-form-field" id={field.label}>
-                                    <Row className="signin-form-checkbox-container">
-                                        <Column
-                                            small={1}
-                                            className="signin-form-input"
-                                        >
-                                            <FormFieldInput
-                                                type={field.type}
-                                                defaultChecked={field.type === 'checkbox' && field.defaultChecked}
-                                                value={field.input}
-                                                onChange={e => setInputValue(field, e)}
-                                                className="signin-form-input-field sign-form-input-checkbox"
-                                            />
-                                        </Column>
-                                        <Column small={6}>
-                                            <FormFieldLabel
-                                                className="signin-form-checkbox-label"
+                            if (field.type === 'checkbox') {
+                                return (
+                                    <FormField
+                                        key={index}
+                                        className="signin-form-field"
+                                        id={field.label.toLowerCase().split(' ').join('-')}
+                                    >
+                                        <Row className="signin-form-checkbox-container">
+                                            <Column
+                                                small={1}
+                                                className="signin-form-input"
                                             >
-                                                {field.label}
-                                            </FormFieldLabel>
-                                        </Column>
-                                        <Column small={5}>
-                                            <a href="#">Forgot Password?</a>
-                                        </Column>
-                                    </Row>
-                                    <Row className="signin-form-submit">
-                                        <Column small={12} className="signin-form-submit-container">
-                                            <Button className="signin-form-submit-button">
-                                                Sign in
-                                            </Button>
-                                        </Column>
-                                    </Row>
-                                </FormField>
-                            )
-                        }
-                        else {
-                            return (
-                                <Row className="signin-form-row" key={index}>
-                                    <Column small={8} smallCentered="centered">
-                                        <FormField key={index} id={field.label} className="signin-form-field">
-                                            <FormFieldLabel></FormFieldLabel>
-                                            <FormFieldInline>
-                                                <FormFieldLabel className="signin-form-label">
-                                                    {
-                                                        field.label === 'Username or email' ?
-                                                            <img src={mail} alt="Email" className="signin-form-mail" /> :
-                                                            <img src={password} alt="Password" className="signin-form-password" />
-                                                    }
-                                                </FormFieldLabel>
                                                 <FormFieldInput
                                                     type={field.type}
+                                                    defaultChecked={field.type === 'checkbox' && field.defaultChecked}
                                                     value={field.input}
                                                     onChange={e => setInputValue(field, e)}
-                                                    className="signin-form-input-field"
-                                                    placeholder={field.label}
+                                                    className="signin-form-input-field sign-form-input-checkbox"
                                                 />
-                                            </FormFieldInline>
-                                        </FormField>
-                                    </Column>
-                                </Row>
-                            )
-                        }
-                    })
-                }
+                                            </Column>
+                                            <Column small={6}>
+                                                <FormFieldLabel
+                                                    className="signin-form-checkbox-label"
+                                                >
+                                                    {field.label}
+                                                </FormFieldLabel>
+                                            </Column>
+                                            <Column small={5}>
+                                                <a href="#">Forgot Password?</a>
+                                            </Column>
+                                        </Row>
+                                    </FormField>
+                                )
+                            }
+                            else {
+                                return (
+                                    <Row className="signin-form-row" key={index}>
+                                        <Column small={8} smallCentered="centered">
+                                            <FormField
+                                                key={index}
+                                                id={field.label.toLowerCase().split(' ').join('-')}
+                                                className="signin-form-field signin-form-field-text-input"
+                                                error={field.validation.errors}
+                                            >
+                                                <FormFieldLabel></FormFieldLabel>
+                                                <FormFieldInline>
+                                                    <FormFieldLabel className="signin-form-label">
+                                                        {
+                                                            field.label === 'Username or email' ?
+                                                                <img src={mail} alt="Email" className="signin-form-mail" /> :
+                                                                <img src={password} alt="Password" className="signin-form-password" />
+                                                        }
+                                                    </FormFieldLabel>
+                                                    <FormFieldInput
+                                                        type={field.type}
+                                                        value={field.input}
+                                                        onChange={e => setInputValue(field, e)}
+                                                        className="signin-form-input-field"
+                                                        placeholder={field.label}
+                                                    />
+                                                </FormFieldInline>
+                                                {
+                                                    field.validation.errorMessages.map((msg, i) => (
+                                                        <FormFieldError
+                                                            key={i}
+                                                            className="signin-form-input-field-error"
+                                                        >
+                                                            {msg}
+                                                        </FormFieldError>
+                                                    ))
+                                                }
+
+                                            </FormField>
+                                        </Column>
+                                    </Row>
+                                )
+                            }
+                        })
+                    }
+                    <Row className="signin-form-submit">
+                        <Column small={12} className="signin-form-submit-container">
+                            <Button
+                                type="submit"
+                                className={signInSubmitButtonClasses.join(' ')}
+                            >
+                                Sign in
+                        </Button>
+                        </Column>
+                    </Row>
+                </form>
+
             </div>
             <Row className="signup">
                 <Column small={12} className="signup-container">
