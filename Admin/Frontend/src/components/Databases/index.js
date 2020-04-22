5362b1ebb589951a5bbf01c8889a43486014d58d
ggornes <gerardogornes@gmail.com>
2020-04-22 16:01:11 +0800

    testing tables in Databases page

--- Admin/Frontend/src/components/Databases/index.js
+++ Admin/Frontend/src/components/Databases/index.js
@@ -11,7 +11,52 @@ import { orderBy } from 'lodash';
 
 export const Databases = () => 
     {
-        const columns = [
+
+        const db_tables_columns = [
+            {
+                name: 'Table Id',
+                selector: 'tableId',
+                sortable: true,
+            },
+            {
+                name: 'Table Name',
+                selector: 'tableName',
+                sortable: true,
+            },
+            {
+                name: 'Created',
+                selector: 'created',
+                sortable: true,
+            },
+            {
+                name: 'Last Update',
+                selector: 'updated',
+                sortable: true,
+            },
+        ];
+
+        const db_tables_rows = [
+            {
+                tableId: 1,
+                tableName: "Users",
+                created: "20/03/2020",
+                updated: "28/03/2020"
+            },
+            {
+                tableId: 2,
+                tableName: "User Features",
+                created: "20/03/2020",
+                updated: "22/03/2020"
+            },
+            {
+                tableId: 3,
+                tableName: "Face Shapes",
+                created: "20/03/2020",
+                updated: "21/03/2020"
+            },
+        ];
+
+        const usersTableColumns = [
             {
                 name: 'Id',
                 selector: 'id',
@@ -44,62 +89,219 @@ export const Databases = () =>
               },
         ]
 
+        const userFeaturesTableColumns = [
+            {
+                name: 'Id',
+                selector: 'id',
+                sortable: true
+            },
+            {
+                name: 'User Id',
+                selector: 'userId',
+                sortable: true
+            },
+            {
+                name: 'Faceshape Id',
+                selector: 'faceShapeId',
+                sortable: true
+            },
+            {
+                name: 'Skin Tone Id',
+                selector: 'skinToneId',
+                sortable: true
+            },
+            {
+                name: 'Hair Style Id',
+                selector: 'hairStyleId',
+                sortable: true
+            },
+            {
+                name: 'Hair Length Id',
+                selector: 'hairLengthId',
+                sortable: true
+            },
+        ];
+
 // DataTable settings
         const [loading, setLoading] = useState(false);
         //const [items, setData] = useState(data);
 
+        // Logic to handle tabel sorting
         const handleSort = (column, sortDirection) => {
             // simulate server sort
             setLoading(true);
         
+            
             // instead of setTimeout this is where you would handle your API call.
             setTimeout(() => {
-              setData(orderBy(data, column.selector, sortDirection));
+              setDBTables(orderBy(DBTables, column.selector, sortDirection));
+              setUsersData(orderBy(usersData, column.selector, sortDirection));
+              setUserFeaturesData(orderBy(userFeaturesData, column.selector, sortDirection));
+
+
               setLoading(false);
             }, 100);
           };
 
+        // Logic to handle selected row
+          const handleChange = (row) => {
+            var tablesToShow = row.selectedRows;
+            
+            console.log("Selected tables: ");
+            console.log(tablesToShow);
+
+            // setSelectedTables(row.SelectedRows);
+
+            // tablesToShow.map((item, index) => {
+            //     console.log(item.tableId);
+            //     switch(item.tableId) {
+            //         case 1:
+            //             setShowUsersTable(!showUsersTable);
+            //         default:
+
+            //     }
+
+            // });
+
+
+
+
+
+
+            setShowUsersTable(!showUsersTable);
+
+            };
+
           const [selectableRows, setSelectableRows] = React.useState(false);
           
-// DataTable settings
+// End of DataTable settings
 
+        // Initialize state variables 
+        const [DBTables, setDBTables] = useState([]);
+        const [usersData, setUsersData] = useState([]);
+        const [showUsersTable, setShowUsersTable] = React.useState(false);
 
-        const [data, setData] = useState([]);
+        const [userFeaturesData, setUserFeaturesData] = useState([]);
+
+        const [selectedTables, setSelectedTables] = useState([]);
+
+        
 
         useEffect(() => {
 
-            const fetchData = async () => {
+            setDBTables(db_tables_rows);
+
+            const fetchUsersTable = async () => {
                 const result = await axios(
                     'https://localhost:5001/api/Users'
                     //'https://128.199.233.190:5001/api/Users'
                 );
 
-                console.log("Result.data:");
+                console.log("Users Table data:");
                 console.log(result.data);
-                setData(result.data);
+                setUsersData(result.data);
             }
 
-            fetchData();
+            const fetchUserFeaturesTable = async () => {
+                const result = await axios(
+                    'https://localhost:5001/api/UserFeatures'
+                );
+
+                console.log("User Features Table data:");
+                console.log(result.data);
+                setUserFeaturesData(result.data);
+            }
+
+            
+
+            fetchUsersTable();
+            fetchUserFeaturesTable();
 
         }, []);
 
         return(
             <div>
                 databases
+                {/* Table that shows all tables on the database */}
+                <div className="db-table-container">
+                    <DataTable
+                        title="hairdress_db Tables"
+                        columns={db_tables_columns}
+                        data={DBTables} // ToDo: Handle dynamycally the tables fom the DB
+                        onSort={handleSort}
+                        sortServer
+                        progressPending={loading}
+                        persistTableHead
+                        highlightOnHover
+                        selectableRows
+                        selectableRowsHighlight
+                        onSelectedRowsChange={handleChange}
+                    />
+                </div>
 
-                <DataTable
+                <div className="selected-table-container">
+                    {/* Users table */}
+                    { showUsersTable ? 
+                    <DataTable
                     title="Users"
-                    columns={columns}
-                    data={data}
+                    columns={usersTableColumns}
+                    data={usersData}
                     onSort={handleSort}
                     selectableRows={selectableRows}
                     sortServer
                     progressPending={loading}
                     persistTableHead
                     />
+                    : 
+                    null }
 
 
-                <div className="database-container">
+                    {/* User Features table */}
+                    <DataTable
+                        title="User Features"
+                        columns={userFeaturesTableColumns}
+                        data={userFeaturesData}
+                        onSort={handleSort}
+                        selectableRows={selectableRows}
+                        sortServer
+                        progressPending={loading}
+                        persistTableHead
+                        />                    
+
+                    {/* <div className="table-btn-container grid-container">
+                        <div className="btn-container grid-x">
+                            <div className="cell small-4">
+                                <Button className="table-btn grid-x">
+                                    <div className="cell small-12">
+                                        <span>Add</span>
+                                    </div>
+                                </Button>
+                            </div>
+
+                            <div className="cell small-4">
+                                <Button className="table-btn grid-x">
+                                    <div className="cell small-12">
+                                        <span>Edit</span>
+                                    </div>
+                                </Button>
+                            </div>
+
+                            <div className="cell small-4">
+                                <Button className="table-btn grid-x">
+                                    <div className="cell small-12">
+                                        <span>Delete</span>
+                                    </div>
+                                </Button>
+                            </div>
+
+                        </div>
+                    </div> */}
+
+                </div>
+
+
+
+                {/* <div className="database-container">
                     <Table scroll>
                         <thead>
                             <tr>
@@ -156,38 +358,8 @@ export const Databases = () =>
                         </tbody>
                     </Table>
 
-                    <div className="table-btn-container grid-container">
-                        <div className="btn-container grid-x">
-                            <div className="cell small-4">
-                                <Button className="table-btn grid-x">
-                                    <div className="cell small-12">
-                                        <span>Add</span>
-                                    </div>
-                                </Button>
-                            </div>
 
-                            <div className="cell small-4">
-                                <Button className="table-btn grid-x">
-                                    <div className="cell small-12">
-                                        <span>Edit</span>
-                                    </div>
-                                </Button>
-                            </div>
-
-                            <div className="cell small-4">
-                                <Button className="table-btn grid-x">
-                                    <div className="cell small-12">
-                                        <span>Delete</span>
-                                    </div>
-                                </Button>
-                            </div>
-
-
-                        </div>
-
-
-                    </div>
-                </div>
+                </div> */}
 
 
 
