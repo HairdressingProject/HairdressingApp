e5b8a707e2975ef26f06297768cbd98c33f65c60
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2020-04-23 23:52:53 +0800

    forgot password page is now responsive

--- Admin/Frontend/src/components/ForgotPassword/index.js
+++ Admin/Frontend/src/components/ForgotPassword/index.js
@@ -49,105 +49,107 @@ export const ForgotPassword = () => {
 
     return (
         <div className={[classes["forgotpassword-container"], "text-center"].join(' ')}>
-            <div className={classes["forgotpassword-form"]}>
-                <Row className={classes["forgotpassword-title-container"]}>
-                    <Column small={12}>
-                        <h1>
-                            Hairdressing Application - Admin Portal
+            <Row>
+                <Column small={12} medium={8} mediumCentered="centered" className={classes["forgotpassword-form"]}>
+                    <Row className={classes["forgotpassword-title-container"]}>
+                        <Column small={12}>
+                            <h1>
+                                Hairdressing Application - Admin Portal
                         </h1>
-                    </Column>
-                </Row>
-                <Row className={classes["forgotpassword-subtitle-container"]}>
-                    <Column small={12}>
-                        <h2>
-                            Enter your registered username or email below to
-                            recover your password.
+                        </Column>
+                    </Row>
+                    <Row className={classes["forgotpassword-subtitle-container"]}>
+                        <Column small={12}>
+                            <h2>
+                                Enter your registered username or email below to
+                                recover your password.
                         </h2>
-                    </Column>
-                </Row>
+                        </Column>
+                    </Row>
 
-                <FormWithValidation
-                    initialFormFields={initialFormFields}
-                    handleSubmit={(e, isFormValid, formFields) => {
-                        // handle form submission here
-                        e.preventDefault();
-                        console.log('forgot password form submitted');
-                        console.log(isFormValid);
-                        console.dir(formFields);
-                    }}
-                    fields={(
-                        formFields,
-                        setInputValue,
-                        setFieldTouched,
-                        isFormValid,
-                        handleBlur) => (
-                            <>
-                                {
-                                    formFields.map((field, index) => (
-                                        <Row className={classes["forgotpassword-form-row"]} key={index}>
-                                            <Column small={8} smallCentered="centered">
-                                                <FormField
-                                                    key={index}
-                                                    id={field.label.toLowerCase().split(' ').join('-')}
-                                                    className={[classes["forgotpassword-form-field"], classes["forgotpassword-form-field-text-input"]].join(' ')}
-                                                    error={field.validation?.some(v => v.error)}
-                                                >
-                                                    <FormFieldLabel></FormFieldLabel>
-                                                    <FormFieldInline>
-                                                        <FormFieldLabel className={classes["forgotpassword-form-label"]}>
-                                                            <img src={mail} alt="Email" className={classes["forgotpassword-form-mail"]} />
-                                                        </FormFieldLabel>
-                                                        <FormFieldInput
-                                                            type={field.type}
-                                                            value={field.input}
-                                                            required={field.required}
-                                                            onChange={e => setInputValue(field, e)}
-                                                            onFocus={() => setFieldTouched(field)}
-                                                            onBlur={e => handleBlur(field, e)}
-                                                            className={classes["forgotpassword-form-input-field"]}
-                                                            placeholder={field.label}
-                                                        />
-                                                    </FormFieldInline>
-                                                    {
-                                                        field
-                                                            .validation
-                                                            .filter(v => v.error)
-                                                            .map(v => v.errorMessage)
-                                                            .map((msg, i) => (
-                                                                <FormFieldError
-                                                                    key={i}
-                                                                    className={classes["forgotpassword-form-input-field-error"]}
-                                                                >
-                                                                    {msg}
-                                                                </FormFieldError>
-                                                            ))
-                                                    }
+                    <FormWithValidation
+                        initialFormFields={initialFormFields}
+                        handleSubmit={(e, isFormValid, formFields) => {
+                            // handle form submission here
+                            e.preventDefault();
+                            console.log('forgot password form submitted');
+                            console.log(isFormValid);
+                            console.dir(formFields);
+                        }}
+                        fields={(
+                            formFields,
+                            setInputValue,
+                            setFieldTouched,
+                            isFormValid,
+                            handleBlur) => (
+                                <>
+                                    {
+                                        formFields.map((field, index) => (
+                                            <Row className={classes["forgotpassword-form-row"]} key={index}>
+                                                <Column small={8} smallCentered="centered">
+                                                    <FormField
+                                                        key={index}
+                                                        id={field.label.toLowerCase().split(' ').join('-')}
+                                                        className={[classes["forgotpassword-form-field"], classes["forgotpassword-form-field-text-input"]].join(' ')}
+                                                        error={field.validation?.some(v => v.error)}
+                                                    >
+                                                        <FormFieldLabel></FormFieldLabel>
+                                                        <FormFieldInline>
+                                                            <FormFieldLabel className={classes["forgotpassword-form-label"]}>
+                                                                <img src={mail} alt="Email" className={classes["forgotpassword-form-mail"]} />
+                                                            </FormFieldLabel>
+                                                            <FormFieldInput
+                                                                type={field.type}
+                                                                value={field.input}
+                                                                required={field.required}
+                                                                onChange={e => setInputValue(field, e)}
+                                                                onFocus={() => setFieldTouched(field)}
+                                                                onBlur={e => handleBlur(field, e)}
+                                                                className={classes["forgotpassword-form-input-field"]}
+                                                                placeholder={field.label}
+                                                            />
+                                                        </FormFieldInline>
+                                                        {
+                                                            field
+                                                                .validation
+                                                                .filter(v => v.error)
+                                                                .map(v => v.errorMessage)
+                                                                .map((msg, i) => (
+                                                                    <FormFieldError
+                                                                        key={i}
+                                                                        className={classes["forgotpassword-form-input-field-error"]}
+                                                                    >
+                                                                        {msg}
+                                                                    </FormFieldError>
+                                                                ))
+                                                        }
 
-                                                </FormField>
-                                            </Column>
-                                        </Row>
-                                    ))
-                                }
-                                <Row className={classes["forgotpassword-form-submit"]}>
-                                    <Column small={12} className={classes["forgotpassword-form-submit-container"]}>
-                                        <Button
-                                            type="submit"
-                                            className={
-                                                isFormValid ?
-                                                    classes["forgotpassword-form-submit-button"] :
-                                                    [classes["forgotpassword-form-submit-button"], classes["forgotpassword-form-submit-button-invalid"]].join(' ')
-                                            }
-                                            disabled={!isFormValid}
-                                        >
-                                            Recover password
+                                                    </FormField>
+                                                </Column>
+                                            </Row>
+                                        ))
+                                    }
+                                    <Row className={classes["forgotpassword-form-submit"]}>
+                                        <Column small={12} className={classes["forgotpassword-form-submit-container"]}>
+                                            <Button
+                                                type="submit"
+                                                className={
+                                                    isFormValid ?
+                                                        classes["forgotpassword-form-submit-button"] :
+                                                        [classes["forgotpassword-form-submit-button"], classes["forgotpassword-form-submit-button-invalid"]].join(' ')
+                                                }
+                                                disabled={!isFormValid}
+                                            >
+                                                Recover password
                                         </Button>
-                                    </Column>
-                                </Row>
-                            </>
-                        )
-                    }
-                />
-            </div>
+                                        </Column>
+                                    </Row>
+                                </>
+                            )
+                        }
+                    />
+                </Column>
+            </Row>
             <Row className={classes["links-container"]}>
                 <Column small={12} className={classes["signin-container"]}>
                     <p className={classes["signin-text"]}>
