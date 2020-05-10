b0002ee20c16e7da31da406c3ca2c5a2e04edfb6
ggornes <gerardogornes@gmail.com>
2020-05-10 10:48:06 +0800

    implementing change user role

--- Admin/Frontend/src/components/Permissions/index.js
+++ Admin/Frontend/src/components/Permissions/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect, useMemo} from 'react';
 
 import DataTable from 'react-data-table-component'
 import { orderBy } from 'lodash';
+import classes from './Permissions.module.scss';
 
-import { useDispatch, useSelector } from 'react-redux';
 import { userActions } from '../../_actions';
 import { Button } from 'react-foundation-components/lib/button';
 import { Row, Column } from 'react-foundation-components/lib/grid';
@@ -11,13 +11,45 @@ import { Row, Column } from 'react-foundation-components/lib/grid';
 import * as DataColumns from '../Databases/Tables/DBTable/DataColumns';
 
 import Modal from 'react-foundation-modal';
+import { FormWithValidation } from '../Forms/FormWithValidation';
+import { FormField, FormFieldLabel, FormFieldInput, FormFieldError } from 'react-foundation-components/lib/forms';
+
+
+import { useDispatch, useSelector } from 'react-redux';
+import { resourceActions } from '../../_actions';
+import { resourceNames } from '../../_constants';
 
 export const Permissions = () => {
+
+    const initialFormFields = [
+        {
+            label: 'Admin',
+            type: 'radio',
+            value: 'admin'
+            
+        },
+        {
+            label: 'Developer',
+            type: 'radio',
+            value: 'developer'
+            
+        },
+        {
+            label: 'User',
+            type: 'radio',
+            value: 'user'
+            
+        }
+    ]
 
     const [isModalOpen, setModalOpen] = useState(false);
     const [tableData, setTableData] = useState([]);
     const [toggleBtn, setToggleBtn] = useState(true);
     const [selectedRow, setSelectedRow] = useState(null);
+    const [isChecked, setChecked] = useState(null);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [objToEdit, setObjToEdit] = useState(null);
+    
 
     const showModal = status => {
         setModalOpen(status);
@@ -38,6 +70,51 @@ export const Permissions = () => {
         }
         console.log("Toggle button ",toggleBtn);
     }
+
+    // *************** onChange
+    const handleChange = e => {
+        setChecked(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("Form submited");
+        console.log(isChecked.toLowerCase());
+        console.log("Object to edit", objToEdit)
+
+        const requestMethod = 'PUT';
+
+        const body = {
+            Id:objToEdit.id, //objToEdit
+            UserRole:isChecked.toLowerCase() //formFields[1].input
+        };
+
+        console.log("submitting form:");
+        console.log(requestMethod);
+        console.dir(body);
+
+        // Submit
+        //const selectedUser = localUsers.users.items.find(uid => uid.id == body.Id);
+
+        // For PUT requests, don't forget to pass the entire resource object
+        // Except for DateModified, since it's automatically set by the database
+        // This is required by ASP.NET Core, because it strictly follows
+        // The HTTP specification
+
+        var resourceObject = {
+            Id: objToEdit.id,
+            UserName: objToEdit.userName,
+            UserEmail: objToEdit.userEmail,
+            UserPassword: "test", //ToDO: remove this field
+            FirstName: objToEdit.firstName,
+            LastName: objToEdit.lastName,
+            UserRole: isChecked.toLowerCase()
+        };
+
+        console.log("Resource object", resourceObject);
+        // dispatch(resourceActions.put(resourceNames.USERS, objToEdit.id, resourceObject));
+
+    };
 
     // Fetch Data ********************
     const [localUsers, setLocalUsers] = useState(null);
@@ -67,6 +144,8 @@ export const Permissions = () => {
     const contextActions = useMemo(() => {
         const handleAction = () => {
             console.log("handling action", selectedRow);
+            console.log("Object to Edit", selectedRow.selectedRows[0]);
+            setObjToEdit(selectedRow.selectedRows[0]);
             // Open change user role modal
             showModal(true);
 
@@ -82,11 +161,6 @@ export const Permissions = () => {
     });
 
 
-    // if (localResources && localResources.resources) {
-    //     if(localResources.resources.users && localResources.resources.users.items && localResources.resources.users.items.users) {
-    //         setTableData(localResources.resources.users.items.users);
-    //     }
-    // }
 
 
     return (
@@ -109,25 +183,127 @@ export const Permissions = () => {
 
                     Change role
                     <>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <Column>
                             <Row>
-                            <input type="radio" id="Admin" name="Admin" value="Admin"/>
+                            <input type="radio" value="Admin" name="userRole" checked={isChecked === "Admin"} onChange={handleChange}/>
                             <label for="Admin">Admin</label>
                             </Row>
                             <Row>
-                            <input type="radio" id="Developer" name="Developer" value="Developer"/>
+                            <input type="radio" value="Developer" name="userRole" checked={isChecked === "Developer"} onChange={handleChange}/>
                             <label for="Developer">Developer</label>
                             </Row>
                             <Row>
-                            <input type="radio" id="User" name="User" value="User"/>
+                            <input type="radio" value="User" name="userRole" checked={isChecked === "User"} onChange={handleChange}/>
                             <label for="User">User</label>
                             </Row>
+                        </Column>
+
+                        <Column>
+                        <Row>
+                        <Button type="submit" disabled={isChecked === null}>Submit</Button>
+                        </Row>
+                        </Column>
+
+                        <Column>
+                        Selected option is : {isChecked}
                         </Column>
                         
                         
                         
                     </form>
+
+
+                    {/* <Row className={classes["change-rol-form-container"]}>
+                        <FormWithValidation
+                            initialFormFields={initialFormFields}
+                            handleSubmit={(e) => {
+                                e.preventDefault();
+
+                                const requestMethod = 'PUT';
+
+                                const body = {
+                                    Id:1, //objToEdit
+                                    UserRole:'admin' //formFields[1].input
+                                };
+
+                                console.log("submitting form:");
+                                console.log(objToEdit);
+                                console.log(requestMethod);
+                                console.dir(body);
+
+                                // Submit
+                                const selectedUser = localUsers.users.items.find(uid => uid.id == body.Id);
+
+                                // For PUT requests, don't forget to pass the entire resource object
+                                // Except for DateModified, since it's automatically set by the database
+                                // This is required by ASP.NET Core, because it strictly follows
+                                // The HTTP specification
+
+                                // var resourceObject = {
+                                //     Id: objToEdit.id,
+                                //     UserName: objToEdit.userName,
+                                //     UserEmail: objToEdit.userEmail,
+                                //     UserPassword: formFields[2].input, //ToDO: remove this field
+                                //     FirstName: objToEdit.firstName,
+                                //     LastName: objToEdit.LastName,
+                                //     UserRole: formFields[0].input
+                                // };
+
+                                // dispatch(resourceActions.put(resourceNames.USERS, body.Id, {
+                                //     Id: body.Id,
+                                //     UserName: body.HairStyleName,
+                                //     HairStyleLinks: selectedHairStyle.hairStyleLinks,
+                                //     DateCreated: selectedHairStyle.dateCreated
+                                // }));
+                            }}
+                            fields ={(
+                                formFields,
+                                setInputValue,
+                                setFieldTouched,
+                                isFormValid,
+                                handleBlur
+                            ) => (
+                                <>
+                                {
+                                    formFields.map(
+                                        (field, index) => {
+                                            return(
+                                                <FormField
+                                                    className={classes["form-field"]}
+                                                    key={index}
+                                                    id={field.label.toLowerCase().split(' ').join('-')}
+
+                                                >
+                                                    <Row>
+                                                        <Column
+                                                            small={12}
+                                                            large={4}
+                                                        >
+                                                            <FormFieldLabel className={classes["form-label"]}>
+                                                                {field.label}
+                                                            </FormFieldLabel>
+                                                        </Column>
+                                                        <Column>
+                                                            <FormFieldInput
+                                                                type={field.type}
+                                                                value={field.value}
+                                                                onChange={e => setChecked(field, e)}
+                                                                className={classes["form-radio"]}
+                                                            />
+                                                        </Column>
+                                                    </Row>
+
+                                                </FormField>
+                                            )                                            
+                                        }
+                                    )
+                                }
+                                </>
+                            )
+                            }
+                        />
+                    </Row> */}
                     </>
 
             </Modal>
