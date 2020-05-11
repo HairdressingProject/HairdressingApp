9b9dfa206e0366219992b40776f54357f88d422c
ggornes <gerardogornes@gmail.com>
2020-05-11 17:43:04 +0800

    Added comments for documentation

--- Admin/Frontend/src/components/Permissions/index.js
+++ Admin/Frontend/src/components/Permissions/index.js
@@ -19,28 +19,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { resourceActions } from '../../_actions';
 import { resourceNames } from '../../_constants';
 
-export const Permissions = () => {
+/**
+ * Permissions page is where the Administrator can change users role.
+ * Contains a <DataTable/> component with an array of users as data with "selectableRows" property.
+ * A <Button/> component that onClick() shows a <Modal/> with a Form to change the user role
+ */
 
-    const initialFormFields = [
-        {
-            label: 'Admin',
-            type: 'radio',
-            value: 'admin'
-            
-        },
-        {
-            label: 'Developer',
-            type: 'radio',
-            value: 'developer'
-            
-        },
-        {
-            label: 'User',
-            type: 'radio',
-            value: 'user'
-            
-        }
-    ]
+export const Permissions = () => {
 
     const [isModalOpen, setModalOpen] = useState(false);
     const [tableData, setTableData] = useState([]);
@@ -51,9 +36,13 @@ export const Permissions = () => {
     const [objToEdit, setObjToEdit] = useState(null);
     
 
+    /**
+     * Function to open and close a <Modal/> component
+     * 
+     * @param {bool} status 
+     */
     const showModal = status => {
         setModalOpen(status);
-        console.log("isModalOpen: ", isModalOpen)
     }
 
         // Refresh the list of items from the database whenever the form is submitted
@@ -68,24 +57,44 @@ export const Permissions = () => {
 
     // ***************** Data Table settings
 
+    /**
+     * Function that sets the "Change role" <Button/> disabled boolean property to true or false
+     * @param {object} row 
+     */
     const handleSelectedrow = (row) => {
         console.log("Selected row: ", row)
-        setSelectedRow(row);
-        if(row.selectedCount > 1)
+        setSelectedRow(row); // update selectedRow state variable
+        if(row.selectedCount > 1) // if more than one element is selected, then disable the button
         {
             setToggleBtn(false);
         }
-        else {
+        else // Enable button only if one element is selected
+        {
             setToggleBtn(true);
         }
         console.log("Toggle button ",toggleBtn);
     }
 
-    // *************** onChange
+    /**
+     * Function that updates the state variable isChecked (null by default)
+     * 
+     * @param {string} e.target.value The string value userRole of radio element 
+     * 
+     */
     const handleChange = e => {
         setChecked(e.target.value);
     };
 
+    /**
+     * Function to handle the change role form submit event
+     * 
+     * @param {} e 
+     * 
+     * @param {string} isChecked value from radio element
+     * @param {obj} objToEdit instance of User class to Edit
+     * @param {string} requestMethod http method
+     * @param {obj} body http request body
+     */
     const handleSubmit = e => {
         e.preventDefault();
         console.log("Form submited");
@@ -95,40 +104,27 @@ export const Permissions = () => {
         const requestMethod = 'PUT';
 
         const body = {
-            Id:objToEdit.id, //objToEdit
+            Id:objToEdit.id,
             UserName: objToEdit.userName,
             UserEmail: objToEdit.userEmail,
-            UserRole:isChecked.toLowerCase() //formFields[1].input
+            UserRole:isChecked.toLowerCase()
         };
 
         console.log("submitting form:");
         console.log(requestMethod);
         console.dir(body);
 
-        // Submit
-        //const selectedUser = localUsers.users.items.find(uid => uid.id == body.Id);
 
         // For PUT requests, don't forget to pass the entire resource object
         // Except for DateModified, since it's automatically set by the database
         // This is required by ASP.NET Core, because it strictly follows
         // The HTTP specification
 
-        var resourceObject = {
-            Id: objToEdit.id,
-            UserName: objToEdit.userName,
-            UserEmail: objToEdit.userEmail,
-            UserPassword: "test", //ToDO: remove this field
-            FirstName: objToEdit.firstName,
-            LastName: objToEdit.lastName,
-            UserRole: isChecked.toLowerCase()
-        };
 
-        console.log("Resource object", resourceObject);
-        // dispatch(resourceActions.put(resourceNames.USERS, objToEdit.id, resourceObject));
         dispatch(userActions.changeUserRole(body));
         setTimeout(() => {
             setFormSubmitted(true);
-            showModal(false);
+            showModal(false); // close modal after submit
         }, 300);
 
     };
