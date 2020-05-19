ea15cb874e8b35dfb1c0a3d1c7146bffa54869cc
ggornes <gerardogornes@gmail.com>
2020-05-19 17:13:19 +0800

    enable http cors policy

--- Admin/Backend/AdminApi/Startup.cs
+++ Admin/Backend/AdminApi/Startup.cs
@@ -98,6 +98,15 @@ namespace AdminApi
                                 .WithMethods("GET", "POST", "PUT", "DELETE")
                                 .WithHeaders("Origin", "Content-Type");
                     });
+
+                options.AddPolicy(name: AllowedOriginsConf,
+                    builder =>
+                    {
+                        builder.AllowCredentials()
+                                .WithOrigins("http://localhost:3000")
+                                .WithMethods("GET", "POST", "PUT", "DELETE")
+                                .WithHeaders("Origin", "Content-Type");
+                    });
             });
         }
 
